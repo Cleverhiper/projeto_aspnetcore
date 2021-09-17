0c3f89712a278388edd5954e9cad915299542d88
Cleverson <cleverhiper@gmail.com>
2021-09-17 08:17:16 -0300

    Projeto com CRUD completo

--- Controllers/FuncionariosController.cs
+++ Controllers/FuncionariosController.cs
@@ -21,12 +21,37 @@ namespace projeto_aspnetcore.Controllers
             return View();
         }
 
+        public IActionResult Editar(int id)
+        {
+            Funcionario funcionario = database.Funcionarios.First(registro => registro.Id == id);
+            return View("Cadastrar",funcionario);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            Funcionario funcionario = database.Funcionarios.First(registro => registro.Id == id);
+            database.Funcionarios.Remove(funcionario);
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         [HttpPost]
         public IActionResult Salvar(Funcionario funcionario) {
-            database.Funcionarios.Add(funcionario);
+            if (funcionario.Id == 0)
+            {
+                //Salvar um novo funcionario
+                database.Funcionarios.Add(funcionario);
+            } else {
+                Funcionario funcionariodoBanco = database.Funcionarios.First(registro => registro.Id == funcionario.Id);
+                funcionariodoBanco.Nome = funcionario.Nome;
+                funcionariodoBanco.Salario = funcionario.Salario;
+                funcionariodoBanco.Cpf = funcionario.Cpf;
+            }
+
             database.SaveChanges();
 
             return RedirectToAction("Index");
         }
+
     }
 }
